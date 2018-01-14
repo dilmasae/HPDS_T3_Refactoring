1fb258829fa14e8b1ef9d1db05dd0c4cc4a25294
Dilma Sae <dilmasae@gmail.com>
2018-01-14 19:29:51 +0000

    Extract Method (solution)

--- src/t3_refactoring/T3_refactoring.java
+++ src/t3_refactoring/T3_refactoring.java
@@ -1,21 +1,16 @@
 package t3_refactoring;
 
 public class T3_refactoring {
+    
+    static int sum(int number1, int number2) {
+        return number1 + number2;
+    }
 
     public static void main(String[] args) {
         System.out.println("Refactoring");
         
-        int number1 = 10;
-        int number2 = 5;
-        int sum = number1 + number2;
-        
-        System.out.println("Sum 1: " + sum);
-        
-        number1 = 1;
-        number2 = 1;
-        sum = number1 + number2;
-        
-        System.out.println("Sum 2: " + sum);
+        System.out.println("Sum 1: " + sum(10, 5));
+        System.out.println("Sum 2: " + sum(1, 1));
         
     }
     
