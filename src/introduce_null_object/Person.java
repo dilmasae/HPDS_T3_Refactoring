518c328163fa9831544ff07843d29ca9852bf9b1
Dilma Sae <dilmasae@gmail.com>
2018-01-14 18:49:26 +0000

    Introduce Null Object (problem)

---
+++ src/introduce_null_object/Person.java
@@ -0,0 +1,19 @@
+package introduce_null_object;
+
+public class Person {
+
+    private String name;
+    
+    public Person() {
+        
+    }
+    
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
