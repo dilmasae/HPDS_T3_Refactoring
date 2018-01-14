ecb17f5e56aaf1423d1f27433290b03a309e5d58
Dilma Sae <dilmasae@gmail.com>
2018-01-14 19:28:20 +0000

    Extract Method (problem)

--- src/t3_refactoring/T3_refactoring.java
+++ src/t3_refactoring/T3_refactoring.java
@@ -4,6 +4,19 @@ public class T3_refactoring {
 
     public static void main(String[] args) {
         System.out.println("Refactoring");
+        
+        int number1 = 10;
+        int number2 = 5;
+        int sum = number1 + number2;
+        
+        System.out.println("Sum 1: " + sum);
+        
+        number1 = 1;
+        number2 = 1;
+        sum = number1 + number2;
+        
+        System.out.println("Sum 2: " + sum);
+        
     }
     
 }
