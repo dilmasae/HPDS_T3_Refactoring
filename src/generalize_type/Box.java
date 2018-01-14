24f59ed108d1f10ecf3033ca333f252da4b36dc0
Dilma Sae <dilmasae@gmail.com>
2018-01-14 18:19:46 +0000

    Generalize types (problem)

---
+++ src/generalize_type/Box.java
@@ -0,0 +1,11 @@
+package generalize_type;
+
+public class Box {
+    
+    private Object object;
+    
+    public void set(Object object) { this.object = object; }
+    
+    public Object get() { return object; }
+    
+}
